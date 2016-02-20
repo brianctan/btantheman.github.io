5cefb60bd7df78ca1df40a478596fdd9dbd9a4ac
mikedelcastillo <johnmichaeldc@gmail.com>
2016-02-20 17:08:36 +0800

    json

---
+++ index/index.js
@@ -0,0 +1,19 @@
+var req, play;
+
+function windowLoad(){
+  req = new XMLHttpRequest();
+  req.overrideMimeType("application/json");
+  req.open('GET', 'index/play.json', true);
+  req.onreadystatechange = function(){
+    if(req.readyState == 4 && req.status == "200"){
+      onReqLoad(req.responseText);
+    }
+  }
+  req.send(null);
+}
+
+window.addEventListener("load", windowLoad, false);
+
+function onReqLoad(data){
+  play = JSON.parse(data);
+}
