63086587646839964b6ee1fef26fa00f98ded9cd
mikedelcastillo <johnmichaeldc@gmail.com>
2016-02-22 18:59:26 +0800

    css updates

--- index/index.js
+++ index/index.js
@@ -1,7 +1,10 @@
-var play;
+var play, ham, nav, navListener;
 
 function windowLoad(){
-  $.getJSON('index/play.json', onReqLoad);
+  //$.getJSON('index/play.json', onReqLoad);
+  ham = document.getElementById("hamburger");
+  nav = document.getElementById("navbar");
+  ham.addEventListener("click", toggleNavigation, false);
 }
 
 window.addEventListener("load", windowLoad, false);
@@ -14,5 +17,13 @@ function onReqLoad(data){
 function displayList(list){
   for(var i in list){
     var item = list[i];
+  }
+}
+
+function toggleNavigation(){
+  if(nav.className == "navigation hidden"){
+    nav.className = "navigation";
+  } else{
+    nav.className = "navigation hidden";
   }
 }
