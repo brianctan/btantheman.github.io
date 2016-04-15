2b9d164226eb1c09c70a06c026f4feff6414d268
mikedelcastillo <johnmichaeldc@gmail.com>
2016-04-15 09:08:34 +0800

    ases

--- play/ASES/js/main.js
+++ play/ASES/js/main.js
@@ -6,3 +6,55 @@ window.addEventListener("load", function(){
     e.className = e.className.replace(" pre", "");
   }
 }, false);
+
+window.addEventListener("load", setLinkFunctions, false);
+
+var linkClickables = [];
+var stopScrolling = true;
+var scrollTarget = 0;
+var windowScroll = 0;
+
+function setLinkFunctions(){
+  var links = document.getElementsByTagName("a");
+  for(var i = 0; i < links.length; i++){
+    var link = links[i];
+    var href = link.getAttribute("href") || "";
+    var hrefr = href.replace("#", "");
+    if(hrefr != href){
+      linkClickables.push(hrefr);
+      link.setAttribute("data-link-id", hrefr);
+      link.addEventListener("click", smoothScroll, false);
+    }
+  }
+
+  document.addEventListener("mousewheel", smoothScrollStop, false);
+  document.addEventListener("touchstart", smoothScrollStop, false);
+  document.addEventListener("mousedown", smoothScrollStop, false);
+  document.addEventListener("keydown", smoothScrollStop, false);
+
+  document.addEventListener("scroll", function(){
+    windowScroll = document.body.scrollTop;
+  }, false);
+}
+
+function smoothScrollStop(){
+  if(!stopScrolling) stopScrolling = true;
+}
+
+function smoothScrollUpdate(){
+  windowScroll += ((scrollTarget - windowScroll)/15);
+  document.body.scrollTop = Math.round(windowScroll);
+  if(Math.round(windowScroll - scrollTarget) == 0) stopScrolling = true;
+  if(!stopScrolling) window.requestAnimationFrame(smoothScrollUpdate);
+}
+
+function smoothScroll(e){
+  var goto = document.getElementById(this.getAttribute("data-link-id"));
+  console.log(goto);
+  if(goto){
+    scrollTarget = goto.offsetTop;
+    stopScrolling = false;
+    smoothScrollUpdate();
+  }
+  e.preventDefault();
+}
